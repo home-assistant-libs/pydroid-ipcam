37f8c5541b870bc583b10a0ec446d4e9d69cce16
pvizeli <pvizeli@syshack.ch>
2017-03-08 12:00:02 +0100

    Initial import pydroid-ipcam

---
+++ README.rst
@@ -0,0 +1,10 @@
+PyDroid-IpCam
+============
+A python asyncio library for handling android ipcam.
+
+Original written for Home-Assistant
+
+Links
+-----
+- https://play.google.com/store/apps/details?id=com.pas.webcam
+- http://home-assistant.io
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+VERSION = "0.1"
+
+setup(
+    name='pydroid-ipcam',
+    version=VERSION,
+    license='BSD License',
+    author='Pascal Vizeli',
+    author_email='pvizeli@syshack.ch',
+    url='https://github.com/pvizeli/pydroid-ipcam',
+    download_url='https://github.com/pvizeli/pydroid-ipcam/tarball/'+VERSION,
+    description=('A asyncio library for handling android ipcam'),
+    long_description=long_description,
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=['android', 'ipcam', 'api', 'asyncio'],
+    zip_safe=False,
+    platforms='any',
+    py_modules=['pydroid-ipcam'],
+    install_requires=[
+        'async_timeout',
+        'aiohttp',
+        'yarl',
+    ],
+)

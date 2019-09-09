c5e73c07c1631984707a8a61eaf120aff2242a8a
Pascal Vizeli <pvizeli@syshack.ch>
2019-09-09 12:19:07 +0000

    Add devops functions

--- pydroid_ipcam.py
+++ pydroid_ipcam.py
@@ -1,134 +1,142 @@
 """PyDroidIPCam api for android ipcam."""
 import asyncio
 import logging
+from typing import Any, Awaitable, Dict, List, Optional, Tuple, Union
 
 import aiohttp
 from yarl import URL
 
 _LOGGER = logging.getLogger(__name__)
 
-ALLOWED_ORIENTATIONS = [
-    'landscape', 'upsidedown', 'portrait', 'upsidedown_portrait'
-]
+ALLOWED_ORIENTATIONS = ["landscape", "upsidedown", "portrait", "upsidedown_portrait"]
 
 
 class PyDroidIPCam:
     """The Android device running IP Webcam."""
 
-    def __init__(self, loop, websession, host, port=8080, username=None,
-                 password=None, timeout=10, ssl=True):
+    def __init__(
+        self,
+        websession: aiohttp.ClientSession,
+        host: str,
+        port: int = 8080,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        timeout: int = 10,
+        ssl: bool = True,
+    ):
         """Initialize the data oject."""
-        self.loop = loop
-        self.websession = websession
+        self.websession: aiohttp.ClientSession = websession
         self.status_data = None
         self.sensor_data = None
-        self._host = host
-        self._port = port
-        self._auth = None
-        self._timeout = None
-        self._available = True
-        self._ssl = ssl
+        self._host: str = host
+        self._port: int = port
+        self._auth: Optional[aiohttp.BasicAuth] = None
+        self._timeout: int = timeout
+        self._available: bool = True
+        self._ssl: bool = ssl
 
         if username and password:
             self._auth = aiohttp.BasicAuth(username, password=password)
 
     @property
-    def base_url(self):
+    def base_url(self) -> str:
         """Return the base url for endpoints."""
         protocol = "https" if self._ssl else "http"
         return f"{protocol}://{self._host}:{self._port}"
 
     @property
-    def mjpeg_url(self):
+    def mjpeg_url(self) -> str:
         """Return mjpeg url."""
         return f"{self.base_url}/video"
 
     @property
-    def image_url(self):
+    def image_url(self) -> str:
         """Return snapshot image url."""
         return f"{self.base_url}/shot.jpg"
 
     @property
-    def available(self):
+    def available(self) -> bool:
         """Return True if is available."""
         return self._available
 
-    async def _request(self, path):
+    async def _request(self, path: str) -> Union[bool, Dict[str, Any]]:
         """Make the actual request and return the parsed response."""
-        url = f'{self.base_url}{path}'
+        url: str = f"{self.base_url}{path}"
         data = None
 
         try:
-            async with self.websession.get(url, auth=self._auth, timeout=self._timeout) as response:
+            async with self.websession.get(
+                url, auth=self._auth, timeout=self._timeout
+            ) as response:
                 if response.status == 200:
-                    if response.headers['content-type'] == 'application/json':
+                    if response.headers["content-type"] == "application/json":
                         data = await response.json()
                     else:
-                        data = await response.text()
+                        data = await response.text().find("Ok") != -1
 
         except (asyncio.TimeoutError, aiohttp.ClientError) as error:
-            _LOGGER.error('Failed to communicate with IP Webcam: %s', error)
+            _LOGGER.error("Failed to communicate with IP Webcam: %s", error)
             self._available = False
-            return
+        else:
+            self._available = True
 
-        self._available = True
-        if isinstance(data, str):
-            return data.find("Ok") != -1
         return data
 
-    async def update(self):
+    async def update(self) -> None:
         """Fetch the latest data from IP Webcam."""
-        status_data = await self._request('/status.json?show_avail=1')
+        status_data = await self._request("/status.json?show_avail=1")
 
-        if status_data:
-            self.status_data = status_data
+        if not status_data:
+            return
 
-            sensor_data = await self._request('/sensors.json')
-            if sensor_data:
-                self.sensor_data = sensor_data
+        self.status_data = status_data
+
+        sensor_data = await self._request("/sensors.json")
+        if sensor_data:
+            self.sensor_data = sensor_data
 
     @property
-    def current_settings(self):
+    def current_settings(self) -> Dict[str, Any]:
         """Return dict with all config include."""
         settings = {}
         if not self.status_data:
             return settings
 
-        for (key, val) in self.status_data.get('curvals', {}).items():
+        for (key, val) in self.status_data.get("curvals", {}).items():
             try:
                 val = float(val)
             except ValueError:
                 val = val
 
-            if val in ('on', 'off'):
-                val = (val == 'on')
+            if val in ("on", "off"):
+                val = val == "on"
 
             settings[key] = val
 
         return settings
 
     @property
-    def enabled_sensors(self):
+    def enabled_sensors(self) -> List[str]:
         """Return the enabled sensors."""
         if self.sensor_data is None:
             return []
         return list(self.sensor_data.keys())
 
     @property
-    def enabled_settings(self):
+    def enabled_settings(self) -> List[str]:
         """Return the enabled settings."""
         if self.status_data is None:
             return []
-        return list(self.status_data.get('curvals', {}).keys())
+        return list(self.status_data.get("curvals", {}).keys())
 
     @property
-    def available_settings(self):
+    def available_settings(self) -> Dict[str, List[Any]]:
         """Return dict of lists with all available config settings."""
         available = {}
         if not self.status_data:
             return available
 
-        for (key, val) in self.status_data.get('avail', {}).items():
+        for (key, val) in self.status_data.get("avail", {}).items():
             available[key] = []
             for subval in val:
                 try:
@@ -136,21 +144,21 @@ class PyDroidIPCam:
                 except ValueError:
                     subval = subval
 
-                if val in ('on', 'off'):
-                    subval = (subval == 'on')
+                if val in ("on", "off"):
+                    subval = subval == "on"
 
                 available[key].append(subval)
 
         return available
 
-    def export_sensor(self, sensor):
+    def export_sensor(self, sensor) -> Tuple(str, Any):
         """Return (value, unit) from a sensor node."""
         value = None
         unit = None
         try:
             container = self.sensor_data.get(sensor)
-            unit = container.get('unit')
-            data_point = container.get('data', [[0, [0.0]]])
+            unit = container.get("unit")
+            data_point = container.get("data", [[0, [0.0]]])
             if data_point and data_point[0]:
                 value = data_point[0][-1][0]
         except (ValueError, KeyError, AttributeError):
@@ -158,108 +166,67 @@ class PyDroidIPCam:
 
         return (value, unit)
 
-    def change_setting(self, key, val):
-        """Change a setting.
-
-        Return a coroutine.
-        """
+    def change_setting(self, key: str, val: Union[str, int, bool]) -> Awaitable[bool]:
+        """Change a setting."""
         if isinstance(val, bool):
-            payload = 'on' if val else 'off'
+            payload = "on" if val else "off"
         else:
             payload = val
-        return self._request(f'/settings/{key}?set={payload}')
+        return self._request(f"/settings/{key}?set={payload}")
 
-    def torch(self, activate=True):
-        """Enable/disable the torch.
-
-        Return a coroutine.
-        """
-        path = '/enabletorch' if activate else '/disabletorch'
+    def torch(self, activate: bool = True) -> Awaitable[bool]:
+        """Enable/disable the torch."""
+        path = "/enabletorch" if activate else "/disabletorch"
         return self._request(path)
 
-    def focus(self, activate=True):
-        """Enable/disable camera focus.
-
-        Return a coroutine.
-        """
-        path = '/focus' if activate else '/nofocus'
+    def focus(self, activate: bool = True) -> Awaitable[bool]:
+        """Enable/disable camera focus."""
+        path = "/focus" if activate else "/nofocus"
         return self._request(path)
 
-    def record(self, record=True, tag=None):
-        """Enable/disable recording.
-
-        Return a coroutine.
-        """
-        path = '/startvideo?force=1' if record else '/stopvideo?force=1'
+    def record(self, record: bool = True, tag: str = None) -> Awaitable[bool]:
+        """Enable/disable recording."""
+        path = "/startvideo?force=1" if record else "/stopvideo?force=1"
         if record and tag is not None:
-            path = f'/startvideo?force=1&tag={URL(tag).raw_path}'
-
+            path = f"/startvideo?force=1&tag={URL(tag).raw_path}"
         return self._request(path)
 
-    def set_front_facing_camera(self, activate=True):
-        """Enable/disable the front-facing camera.
+    def set_front_facing_camera(self, activate: bool = True) -> Awaitable[bool]:
+        """Enable/disable the front-facing camera."""
+        return self.change_setting("ffc", activate)
 
-        Return a coroutine.
-        """
-        return self.change_setting('ffc', activate)
+    def set_night_vision(self, activate: bool = True) -> Awaitable[bool]:
+        """Enable/disable night vision."""
+        return self.change_setting("night_vision", activate)
 
-    def set_night_vision(self, activate=True):
-        """Enable/disable night vision.
+    def set_overlay(self, activate: bool = True) -> Awaitable[bool]:
+        """Enable/disable the video overlay."""
+        return self.change_setting("overlay", activate)
 
-        Return a coroutine.
-        """
-        return self.change_setting('night_vision', activate)
+    def set_gps_active(self, activate: bool = True) -> Awaitable[bool]:
+        """Enable/disable GPS."""
+        return self.change_setting("gps_active", activate)
 
-    def set_overlay(self, activate=True):
-        """Enable/disable the video overlay.
+    def set_quality(self, quality: int = 100) -> Awaitable[bool]:
+        """Set the video quality."""
+        return self.change_setting("quality", quality)
 
-        Return a coroutine.
-        """
-        return self.change_setting('overlay', activate)
+    def set_motion_detect(self, activate: bool = True) -> Awaitable[bool]:
+        """Set motion detection on/off."""
+        return self.change_setting("motion_detect", activate)
 
-    def set_gps_active(self, activate=True):
-        """Enable/disable GPS.
-
-        Return a coroutine.
-        """
-        return self.change_setting('gps_active', activate)
-
-    def set_quality(self, quality=100):
-        """Set the video quality.
-
-        Return a coroutine.
-        """
-        return self.change_setting('quality', quality)
-
-    def set_motion_detect(self, activate=True):
-        """Set motion detection on/off.
-        Return a coroutine.
-        """
-        return self.change_setting('motion_detect', activate)
-
-    def set_orientation(self, orientation='landscape'):
-        """Set the video orientation.
-
-        Return a coroutine.
-        """
+    def set_orientation(self, orientation: str = "landscape") -> Awaitable[bool]:
+        """Set the video orientation."""
         if orientation not in ALLOWED_ORIENTATIONS:
-            _LOGGER.debug('%s is not a valid orientation', orientation)
-            return False
-        return self.change_setting('orientation', orientation)
+            raise RuntimeError(f"Invalid orientation {orientation}")
+        return self.change_setting("orientation", orientation)
 
-    def set_zoom(self, zoom):
-        """Set the zoom level.
+    def set_zoom(self, zoom: int) -> Awaitable[bool]:
+        """Set the zoom level."""
+        return self._request(f"/settings/ptz?zoom={zoom}")
 
-        Return a coroutine.
-        """
-        return self._request(f'/settings/ptz?zoom={zoom}')
-
-    def set_scenemode(self, scenemode='auto'):
-        """Set the video scene mode.
-
-        Return a coroutine.
-        """
-        if scenemode not in self.available_settings['scenemode']:
-            _LOGGER.debug('%s is not a valid scenemode', scenemode)
-            return False
-        return self.change_setting('scenemode', scenemode)
+    def set_scenemode(self, scenemode: str = "auto") -> Awaitable[bool]:
+        """Set the video scene mode."""
+        if scenemode not in self.available_settings["scenemode"]:
+            raise RuntimeError(f"Invalid scene mode {scenemode}")
+        return self.change_setting("scenemode", scenemode)
